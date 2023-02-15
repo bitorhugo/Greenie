5f62c6c0931b65df9ad0065577b9e9afca9d89dc
bitorhugo <vitorhugosantos144@gmail.com>
2023-02-15 19:46:34 +0000

    fixed calling of openai scripts

--- ftm.sh
+++ ftm.sh
@@ -8,8 +8,8 @@ if [ $# -lt 2 ]; then
 fi
    
 # check for openai CLI
-FILE="${HOME}"/.local/bin/openai
-if [[ ! -f "$FILE" ]]; then
+openai="${HOME}"/.local/bin/openai
+if [[ ! -f "$openai" ]]; then
     echo "OpenAI CLI not found!"
     pip install --upgrade openai
 fi
@@ -25,10 +25,11 @@ for ((i=0; $i < $count; i++)); do
     fi
 done
 if [[ ! ${flag} ]]; then
+    echo "Error: Invalid train file given.."
     exit 1
 fi
 # prepare data or training
-exec ${HOME}/.local/bin/openai tools fine_tunes.prepare_data -f $2
+python ${openai} tools fine_tunes.prepare_data -f "$2"
 
 # create fine tuned model
 echo "Creating Model.."
@@ -36,10 +37,10 @@ if [[ ! -z "$3" ]]; then
     echo "Name: $1"
     echo "File: $2"
     echo "Base-Model: $3"   
-    openai api fine_tunes.create -t data_prepared.jsonl -m "$3" --suffix "$1"
+    python ${openai} api fine_tunes.create -t data_prepared.jsonl -m "$3" --suffix "$1"
 else     # default to davinci
     echo "Name: $1"
     echo "File: $2"
     echo "Base-Model: davinci"   
-    openai api fine_tunes.create -t data_prepared.jsonl -m davinci --suffix "$1"
+    python ${openai} api fine_tunes.create -t data_prepared.jsonl -m davinci --suffix "$1"
 fi
