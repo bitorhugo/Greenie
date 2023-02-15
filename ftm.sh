95b935c1b27d8b0470a68b29ec52f3e86d5d39bf
bitorhugo <vitorhugosantos144@gmail.com>
2023-02-15 01:05:33 +0000

    create new fine tuned model.

---
+++ ftm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# Usage: <script-name> <model-name> <train-file-if-or-path> <base-model>
+
+#check for openai CLI
+FILE=~/.local/bin/openai
+if [[ ! -f "$FILE" ]]; then
+    echo "OpenAI CLI not found!"
+    pip install --upgrade openai
+fi
+
+# create fine tuned model
+echo "Creating Model.."
+
+if [[ ! -z "$3" ]]; then
+    echo "Name: $1"
+    echo "File: $2"
+    echo "Base-Model: $3"   
+    # openai api fine_tunes.create -t "$2" -m "$3" --suffix "$1"
+else     # default to davinci
+    echo "Name: $1"
+    echo "File: $2"
+    echo "Base-Model: davinci"   
+    # openai api fine_tunes.create -t "$2" -m davinci --suffix "$1"
+fi
