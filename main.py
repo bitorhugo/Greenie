d3772cdb052256b2be3d38c33b5b9ec37d76227c
bitorhugo <vitorhugosantos144@gmail.com>
2023-02-15 14:40:10 +0000

    example of text completion

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 
+import os
+import openai
 
-print ('Hello World')
+openai.organization = "org-jOA7rNNtj8vYONYLWZ6egAU0"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+
+print (response)
